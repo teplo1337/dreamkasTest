7d7c11894a900a6d18a78833e4ae3869904ce8f1
teplo1337 <ganulenkoaf@gmail.com>
2017-11-02 15:55:39 +0500

    index html - загрузка только изображений, установлена DB, драйвер, статик сервер, создана публичная папка, настроена загрузка изображений multer

---
+++ server.js
@@ -0,0 +1,41 @@
+const express     = require('express'),
+      path        = require('path'),
+      multer      = require('multer'),
+      pg          = require('pg'),
+      configUriDb = "postgres://dlumhzyl:fpOHOo0WYy14hshJWpYUrShNFZH9LBRq@dumbo.db.elephantsql.com:5432/dlumhzyl",
+      client      = new pg.Client(configUriDb),
+      app         = express();
+
+var storage = multer.diskStorage({                                              //настройка Multera путь и имя
+      destination: function (req, file, cb) {
+        cb(null, "./app/public/uploads/")
+      },
+      filename: function (req, file, cb) {
+        var pathImage = file.fieldname + '-' + Date.now()+ path.extname(file.originalname);
+        cb(null, pathImage)
+      }
+    });
+
+var upload = multer({ storage: storage }).single('image');                      // настройка функци мультера
+app.use(express.static('./app/public'));                                        //статик сервер public
+app.get('/', (req,res) => {                                                     //get '/'
+  res.sendFile(path.resolve("./app/html/index.html"));
+});
+app.post('/', upload, (req,res) => {                                            //post '/'
+  client.connect();
+  client.query('CREATE TABLE IF NOT EXISTS newimgdata(data json)');             //create table in db
+                                                                                //insert in table in db
+  client.query('INSERT INTO newimgdata VALUES (\'{"name": "'+req.body.name+'", "destination":"'+req.file.destination+req.file.filename+'"}\')', function(err, result) {
+
+    console.log(result)
+  });
+
+  client.query("SELECT * FROM newimgdata", function(err, result) {              //select from table in db
+      result.rows.forEach(function (row){
+      console.log(row.data);
+      });
+    });
+});
+app.listen(8080, () => {                                                        //start server
+  console.log('listen 8080');
+});
